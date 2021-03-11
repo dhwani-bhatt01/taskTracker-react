88691f9c33821e1275a9f8ae06e8037510d953d8
Dhwani Bhatt <bhattdhwani01@gmail.com>
2021-03-11 13:27:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import Header from './components/Header'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+        id: 1,
+        text: 'Doctors Appointment',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Meeting at School',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true, 
+    },
+    {
+        id: 3,
+        text: 'Food Shopping',
+        day: 'Feb 5th at 2:30pm',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: 'Studying',
+        day: 'Feb 5th at 2:30pm',
+        reminder: false,
+    }
+])
+
+//Add Task
+const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
 }
 
-export default App;
+//Delete Task
+const deleteTask = (id) => {
+  setTasks(tasks.filter((task) => task.id !== id))
+}
+
+  return (
+    <div>
+      <Header /> 
+      <AddTask onAdd={addTask} />
+      {tasks.length > 0 ? ( <Tasks tasks = {tasks} onDelete = {deleteTask}/>) : ('No Tasks To Show')
+       }
+    </div>
+  )
+}
+
+
+export default App
